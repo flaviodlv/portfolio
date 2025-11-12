2ce704d1120dd7a9e6530f0d84de7593b438e893
Flavio <dolv.flavio@gmail.com>
2025-11-12 22:40:57 +0100

    css modification to navbar

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p class="paragraph">
+      Je suis étudiant en deuxième année de BTS SIO, en alternance.
+    </p>
+    <p class="paragraph">J'ai un intérêt profond pour le web, je m'exerce à faire des projets à côté de l'école et l'entreprise.</p>
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  layouts: "default",
+});
+</script>
+<style scoped>
+.paragraph {
+  font-size: 1.2rem;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+}
+</style>
